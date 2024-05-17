3bf0f70e9a28c0e3bfc6d5bd0e7f79807a4305e5
ArnavW <arnavwaghdhare@gmail.com>
2024-05-18 00:16:46 +0530

    scraper.py

--- Anime_Recommender/scraper.py
+++ Anime_Recommender/scraper.py
@@ -1,29 +1,30 @@
 import requests
-from selenium import webdriver
+#from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import csv
 
-
+#Don't use selenium, not efficient enough
 URL = "https://myanimelist.net/topanime.php"
+#Change the link after running the script.
+#second webpage is https://myanimelist.net/topanime.php?limit=50
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib')
 
-browser = webdriver.Firefox()
-browser.get(URL)
-browseOff = False
+                #browser = webdriver.Firefox()
+                #browser.get(URL)
+                #browseOff = False
 #Allow the page to load and the script to run
 time.sleep(5)
 #Make a csv file that holds the data
 #Current idea is to run the script 50 times and
 #get the next anime in the table and
 #add it to the csv file
-for i in range(1, 51):
 
 
-browseOff = bool(input("------Close webpage?------\n"))
-if browseOff:
-    browser.close()
-    browser.quit()
+                #browseOff = bool(input("------Close webpage?------\n"))
+                #if browseOff:
+                #    browser.close()
+                #   browser.quit()
