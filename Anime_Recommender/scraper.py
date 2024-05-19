837feffdea5bd5a3a414c28ae87af5f4193d5374
ArnavW <arnavwaghdhare@gmail.com>
2024-05-19 23:56:48 +0530

    Final_Day_2

--- Anime_Recommender/scraper.py
+++ Anime_Recommender/scraper.py
@@ -9,19 +9,30 @@ URL = "https://myanimelist.net/topanime.php"
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html5lib')
 #print(soup.prettify())
-print(soup.find_all('tr'))
-#Get the table that holds the anime
-header = []
+#print(soup.find_all('tr'))
 
-table = soup.find('table', attrs = {'class':'table-header'})
-#print(soup.table.prettify())
+#Get the table that holds the anime
+lists = []
+temp_lst = []
+
+#Get a table header list inside the list, as the first list.
+#maybe using Pandas would be better, but idrk how I would implement it....
+table_header = soup.find('tr', attrs={'class':'table-header'})
+for row in table_header.find_all('td'):
+    temp_lst.append(row.text)
+lists.append(temp_lst)
+
+#Now let's get the tr with class in ranking-list
+top_Fifty = soup.find_all('tr', attrs={'class':'ranking-list'})
+for row in top_Fifty:
+    anime = {}
+    for col in row.find('td'):
+        anime[lists[0][0]] = col.text
+    lists.append(temp_lst)
+print(lists)
+
+
 #Make a csv file that holds the data
 #Current idea is to run the script 50 times and
 #get the next anime in the table and
 #add it to the csv file
-
-
-                #browseOff = bool(input("------Close webpage?------\n"))
-                #if browseOff:
-                #    browser.close()
-                #   browser.quit()
