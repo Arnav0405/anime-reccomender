9cfca26bf0754c730e2cc1094f527867b8c8454a
ArnavW <arnavwaghdhare@gmail.com>
2024-05-22 23:34:56 +0530

    final_touches

--- Anime_Recommender/gatherer.py
+++ Anime_Recommender/gatherer.py
@@ -1,50 +1,43 @@
 import requests
 # Here we define our query as a multi-line string
 query = '''
-query($id: Int){
-  Media(id: $id){
-    id
-    title {
-      romaji
-      english
+{
+  Page(page: 1, perPage: 200) {
+    pageInfo {
+      total
+      lastPage
+      hasNextPage
     }
-    popularity
-    relations {
-      edges {
-        id
+    media(type: ANIME, popularity_greater: 10000, sort: POPULARITY_DESC) {
+      id
+      title {
+        romaji
+        english
       }
-    }
-    genres
-    averageScore
-    staff(page: 1){
-      edges {
-        node {
-          id
-          name {
-            full
-          }
-          languageV2
+      startDate {
+        year
+      }
+      popularity
+      favourites
+      tags {
+        name
+      }
+      genres
+      averageScore
+      Description
+      studios(isMain: true) {
+        nodes{
+          name
         }
-        role 
-      }
-    }
-    characters(page: 1){
-      edges {
-        node {
-          id 
-          name {
-          full
-          }
-        } 
-      }
+      } 
     }
   }
 }
+
 '''
 
 # Define our query variables and values that will be used in the query request
 variables = {
-    'id': 158927
 }
 
 url = 'https://graphql.anilist.co'
